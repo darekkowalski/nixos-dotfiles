003f14ec8a55136ed53ccf4d683034810c5258b3
Darek Kowalski <darek@module17.com>
2019-08-16 15:06:49 -0400

    Set home path to dk

--- .bin/set-wallpaper.sh
+++ .bin/set-wallpaper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wallpapers="/home/darek/Pictures/wallpapers/"
+wallpapers="/home/dk/Pictures/wallpapers/"
 
 img="$wallpapers`ls $wallpapers | sort -R | tail -$N | head -n 1`"
 
